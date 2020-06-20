75beefda91299235e6e152ae0aca803b2c31cf59
jp <y.polanco12@gmail.com>
2020-06-20 13:27:09 -0400

    hosting here for now

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/******************************************************************************************************************/
+/* BRAX | Responsive Personal Portfolio Template                                                                  */
+/* (c) 2018 Rene Puchinger                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["I am Web Developer.", "I am a Problem Solver.","I am Creative." ,"I am a Software Engineer!"];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable typed effect  */
+var config_typed = true;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
